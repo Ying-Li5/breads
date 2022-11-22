6f867b96f033cc48c1df7c2dbcdebc77ff5837d6
Ying <ying__li@hotmail.com>
2022-11-21 19:48:56 -0500

    sending index and show data

--- server.js
+++ server.js
@@ -6,19 +6,25 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
-// ROUTES
-app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
-})
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 // BREADS
 const breadsController = require('./controllers/bread_controllers.js')
 app.use('/breads', breadsController)
 
-// MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an Awesome App about Breads!')
+})
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
 
 // LISTEN
 app.listen(process.env.PORT)
